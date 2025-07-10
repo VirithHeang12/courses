2ec3cc6930b220dda21dd49e24d3b5663252f442
Heang Virith <virith.id@gmail.com>
2025-07-10 21:54:39 +0700

    Implement Prisma for authentication and database management; add necessary migrations and update configuration files.

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,20 +1,30 @@
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 import { NuxtAuthHandler } from '#auth'
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 export default NuxtAuthHandler({
-    // A secret string you define, to ensure correct encryption
-    secret: useRuntimeConfig().authSecret,
-    providers: [
-        // @ts-expect-error Use .default here for it to work during SSR.
-        GithubProvider.default({
-            clientId: useRuntimeConfig().authGithubClientId,
-            clientSecret: useRuntimeConfig().authGithubClientSecret
-        }),
-        // @ts-expect-error Use .default here for it to work during SSR.
-        GoogleProvider.default({
-            clientId: useRuntimeConfig().authGoogleClientId,
-            clientSecret: useRuntimeConfig().authGoogleClientSecret
-        })
-    ]
+	// Use Prisma adapter for NextAuth.js
+	adapter: PrismaAdapter(prisma),
+
+	// A secret string you define, to ensure correct encryption
+	secret: useRuntimeConfig().authSecret,
+	providers: [
+		// @ts-expect-error Use .default here for it to work during SSR.
+		GithubProvider.default({
+			clientId: useRuntimeConfig().authGithubClientId,
+			clientSecret: useRuntimeConfig().authGithubClientSecret
+		}),
+		// @ts-expect-error Use .default here for it to work during SSR.
+		GoogleProvider.default({
+			clientId: useRuntimeConfig().authGoogleClientId,
+			clientSecret: useRuntimeConfig().authGoogleClientSecret
+		})
+	],
+	session: {
+		strategy: 'database'
+	}
 })
